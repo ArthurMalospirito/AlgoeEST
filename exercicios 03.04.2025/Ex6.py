e169c92a4cd7dd2954f99fbfd3bcc1146508f57f
Arthur Alvarenga Malospirito <arthur.escola.200608@gmail.com>
2025-04-03 14:37:47 -0400

    Delete exercicios 03.04.2025 directory

--- exercicios 03.04.2025/Ex6.py
+++
@@ -1,7 +0,0 @@
-salarioBase = float(input("Digite seu salário:\n"))
-horasExtras = int(input("Digite a quantidade de horas extras trabalhadas:\n"))
-valorHoraExtra = float(input("Digite o valor da hora extra:\n"))
-
-salarioTotal = salarioBase+(horasExtras*valorHoraExtra)
-
-print(f"Seu salário com as horas extras é de R$ {salarioTotal}")
